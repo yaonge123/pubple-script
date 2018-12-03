77572ab9fb660d33d323a671e99d807dabeb7347
Yena <yaonge_01@naver.com>
2018-12-03 10:47:27 +0900

    비상 dvd 개별 페이지 btn.js 수정

--- dvd/visang/element/he/ebook/btn.js
+++ dvd/visang/element/he/ebook/btn.js
@@ -1,12 +1,35 @@
 $(document).ready(function() {
-	// 팝업 띄우기 버튼
-	$(".popBtncont").on("click", function() {
-		var targetUrl = $(this).data("src");
-		var targetPopup = $(this).data("pop");
-		var pageinfo = this.id.split("_");
-		var page = pageinfo[1];
+	// 팝업 버튼
+	var data = DATA.listInfo;
 
-		parent.viewer.syncPageViewGA("팝업+초등학교 실과5+" + page + "_" + targetPopup + ".html");
-		parent.viewer.link('popup', '../../data/' + targetUrl);
+	$(".popBtncont").on("click", function() {
+		var page = this.id;
+		var pageinfo = this.id.split("_");
+		var btnChap = pageinfo[0].substring(2);
+		var pageNum = pageinfo[1];
+		var i = 0, j = 0, k = 0;
+		var chapter, unit, section, file, title;
+
+		for(; i < data.length; i++){
+			if (btnChap - 1 === i) {
+				chapter = data[i];
+				unit = chapter.unit;
+
+				for (; j < unit.length; j++) {
+					section = unit[j].section;
+					k = 0;
+
+					for (; k < section.length; k++) {
+						file = section[k].file;
+						title = section[k].title;
+
+						if (file === page) {
+							parent.viewer.syncPageViewGA("팝업+초등학교 실과5+" + pageNum + "_" + title + ".html");
+							return;
+						}
+					}
+				}
+			}
+		}
 	});
 });
--- dvd/visang/element/pe/ebook/btn.js
+++ dvd/visang/element/pe/ebook/btn.js
@@ -1,12 +1,38 @@
 $(document).ready(function() {
-	// 팝업 띄우기 버튼
-	$(".popBtncont").on("click", function() {
-		var targetUrl = $(this).data("src");
-		var targetPopup = $(this).data("pop");
-		var pageinfo = this.id.split("_");
-		var page = pageinfo[1];
+	// 팝업 버튼
+	var data = DATA.listInfo;
 
-		parent.viewer.syncPageViewGA("팝업+초등학교 체육5+" + page + "_" + targetPopup + ".html");
-		parent.viewer.link('popup', '../../data/' + targetUrl);
+	$(".popBtncont").on("click", function() {
+		var page = this.id;
+		var pageinfo = this.id.split("_");
+		var btnChap = pageinfo[0].substring(2);
+		var pageNum = pageinfo[1];
+		var i = 0, j = 0, k = 0, n = 0;
+		var chapter, unit, sub, section, file, title;
+
+		for(; i < data.length; i++){
+			if (btnChap - 1 === i) {
+				chapter = data[i];
+				unit = chapter.unit;
+
+				for (; j < unit.length; j++) {
+					sub = unit[j].sub;
+					for (; k < sub.length; k++){
+						section = sub[k].section;
+						n = 0;
+						
+						for (; n < section.length; n++) {
+							file = section[n].file;
+							title = section[n].title;
+	
+							if (file === page) {
+								parent.viewer.syncPageViewGA("팝업+초등학교 체육5+" + pageNum + "_" + title + ".html");
+								return;
+							}
+						}
+					}
+				}
+			}
+		}
 	});
 });
